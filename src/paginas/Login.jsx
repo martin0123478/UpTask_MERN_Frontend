6fbed1c05caf49d116fd8af2498d914688daf1fe
martin0123478 <martin_agui97@outlook.es>
2023-04-14 14:25:23 -0600

    feat:Create auth form

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -2,9 +2,36 @@
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
+   <>
+   <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesión y administra  tus 
+  {''}   <span className="text-slate-700">proyectos</span>
+    </h1>
+
+    <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+        <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="email"
+            >Email</label>
+            <input type="email" 
+            id="email"
+            placeholder="Email de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
+        </div>
+
+         <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold"
+            htmlFor="password"
+            >Password</label>
+            <input type="password" 
+            id="password"
+            placeholder="Password de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
+        </div>
+        <input type="submit" value="Iniciar Sesión" 
+        className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded mb-5
+        hover:cursor-pointer hover:bg-sky-800 transition-colors"/>
+    </form>
+   </>
   )
 }
 

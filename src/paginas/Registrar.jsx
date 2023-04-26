9c67969601e794ca8416685e2925e14ba6690bd3
martin0123478 <martin_agui97@outlook.es>
2023-04-25 23:02:38 -0600

    feat:validate all camps in register form

--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,21 +1,34 @@
 import {useState} from 'react'
 import {Link} from 'react-router-dom'
+import Alerta from '../components/Alerta'
 
 const Registrar = () => {
   const [nombre,setNombre] = useState('')
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
   const [repetiPassword,setRepetirPassword] = useState('')
+  const [alerta,setAlerta] = useState({})
 
+const handleSubmit = e =>{
+  e.preventDefault()
+  if([nombre,email,password,repetiPassword].includes('')){
+    setAlerta({
+      msg:'todos los campos son obligatorios',
+      error:true
+    })
+    return
+  }
+}
 
+const {msg} = alerta
 
   return (
     <>
    <h1 className="text-sky-600 font-black text-6xl capitalize">Crea tu cuenta y administra tus
   {''}   <span className="text-slate-700">proyectos</span>
     </h1>
-
-    <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+    {msg && <Alerta alerta={alerta}/>}
+    <form className="my-10 bg-white shadow rounded-lg px-10 py-5" onSubmit={handleSubmit}>
        <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold"
             htmlFor="nombre"

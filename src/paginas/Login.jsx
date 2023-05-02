ec4d274cfceedc689db7bfdb308a17f7075cb09e
martin0123478 <martin_agui97@outlook.es>
2023-05-02 13:27:38 -0600

    feat:validate login form

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,13 +1,40 @@
-import {Link} from 'react-router-dom'
+import {useState} from 'react'
+import {Link,Navigate} from 'react-router-dom'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/clienteAxios'
 
 const Login = () => {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [alerta,setAlerta] = useState({})
+
+    const handleSubmit = async e =>{
+        e.preventDefault()
+        if([email,password].includes('')){
+            setAlerta({
+                msg:'Todos los campos son obligatorios',
+                error:true
+            })
+            return
+        }
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    const {msg} = alerta
   return (
    <>
    <h1 className="text-sky-600 font-black text-6xl capitalize">Inicia sesión y administra  tus 
   {''}   <span className="text-slate-700">proyectos</span>
     </h1>
 
-    <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+    {msg && <Alerta alerta={alerta}/>}
+
+    <form className="my-10 bg-white shadow rounded-lg px-10 py-5"
+    onSubmit={handleSubmit}>
         <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold"
             htmlFor="email"
@@ -15,7 +42,10 @@ const Login = () => {
             <input type="email" 
             id="email"
             placeholder="Email de registro"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={e =>setEmail(e.target.value)}
+            />
         </div>
 
          <div className="my-5">
@@ -25,11 +55,16 @@ const Login = () => {
             <input type="password" 
             id="password"
             placeholder="Password de registro"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"/>
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={password}
+            onChange={e =>setPassword(e.target.value)}
+            
+            />
         </div>
         <input type="submit" value="Iniciar Sesión" 
         className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded mb-5
-        hover:cursor-pointer hover:bg-sky-800 transition-colors"/>
+        hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
     </form>
     <nav className="lg:flex lg:justify-between">
         <Link

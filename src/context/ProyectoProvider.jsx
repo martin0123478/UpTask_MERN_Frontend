c01d1987918bc25f9f7f3e738548c9aa9e199da3
martin0123478 <martin_agui97@outlook.es>
2023-05-04 14:00:45 -0600

    feat:delete project and sync state

--- src/context/ProyectoProvider.jsx
+++ src/context/ProyectoProvider.jsx
@@ -129,8 +129,31 @@ const ProyectoProvider = ({children}) =>{
     }
 
     const eliminarProyecto = async id =>{
-        console.log('eliminando',id)
+       try {
+         const token = localStorage.getItem('token')
+            if(!token) return
+            const config ={
+                headers:{
+                    "Content-Type":"application/json",
+                    Authorization:`Bearer ${token}`
+                }
+            }  
+            const {data} = await clienteAxios.delete(`/proyectos/${id}`,config)
+            const proyectosActualizados = proyectos.filter(proyectoState =>proyectoState._id != id)
+            setProyectos(proyectosActualizados)
+            setAlerta({
+                msg:data.msg,
+                error:false
+            })
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 2000);
+       } catch (error) {
+        console.log(error)
+       }
     }
+
     return(
         <ProyectoContext.Provider
         value={{
